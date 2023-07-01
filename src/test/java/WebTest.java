08a6924c4befdd1ce9959b64153f6c91b02aa6b6
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-07-01 20:37:27 +0300

    v4

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,34 +1,30 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-       WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "driver/LICENSE.chromedriver");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setup() {
-        driver = WebDriverManager.chromedriver().create();
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -39,14 +35,18 @@ public class WebTest {
     }
 
     @Test
-    void test() throws InterruptedException {
-      driver.get("http://localhost:9999");
-        WebElement form= driver.findElement(By.cssSelector("[form form_size_m form_theme_alfa-on-white]"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Иванов");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79059654525");
-        form.findElement(By.cssSelector("[button button_view_extra button_size_m button_theme_alfa-on-white] input")).click();
-        Thread.sleep(5000);
+    void WebTest() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+78888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals(expected,actual);
+
     }
+
+
 
 }
 
